48b676db0d4842d3558d51febd193be3258016aa
funnsam <funnsam8@gmail.com>
2025-01-14 04:41:00 +0000

    convoluted convolution impls

--- src/ops.rs
+++ src/ops.rs
@@ -245,6 +245,68 @@ impl HVector<3> {
     }
 }
 
+macro_rules! convolution {
+    (for_in $p:tt $k:tt $pv:tt $kv:tt ; $wsum:tt $self:tt $kernel:tt $($rp:tt $rk:tt $rpv:tt $rkv:tt),*) => {
+        for $kv in $pv.saturating_sub($k)..$pv.min($p) {
+            $wsum += $self[[$($rkv),*]] * $kernel[[$($rpv - $rkv - 1),*]];
+        }
+    };
+    (for_in $p:tt $k:tt $pv:tt $kv:tt , $($r:tt)*) => {
+        for $kv in $pv.saturating_sub($k)..$pv.min($p) {
+            convolution!(for_in $($r)*);
+        }
+    };
+    (for_out $stride:expr, $p:tt $k:tt $pv:tt ; $self:tt $kernel:tt $new:tt $i:tt $($rp:tt $rk:tt $rpv:tt $rkv:tt),*) => {
+        for $pv in 1..($p + $k + $stride[0] - 1) / $stride[0] {
+            let $pv = ($pv - 1) * $stride[0] + 1;
+            let mut wsum = 0.0;
+            convolution!(for_in $($rp $rk $rpv $rkv),* ; wsum $self $kernel $($rp $rk $rpv $rkv),*);
+            $new.inner[$i] = wsum;
+            $i += 1;
+        }
+    };
+    (for_out $stride:expr, $p:tt $k:tt $pv:tt , $($r:tt)*) => {
+        for $pv in 1..($p + $k + $stride[0] - 1) / $stride[0] {
+            let $pv = ($pv - 1) * $stride[0] + 1;
+            convolution!(for_out &$stride[1..], $($r)*);
+        }
+    };
+    ($tensor:tt $D:tt $dim:tt $($i:tt $p:tt $k:tt $b:tt $pv:tt $kv:tt),*) => {
+        impl<$(const $p: usize),*> $tensor<$($p),*> where bound!($D<$($p),*>): Sized {
+            /// Computes the convolution.
+            ///
+            /// # Note
+            /// Each dimension of the output must be `ceil((self + kernel) / stride) - 1` or else
+            /// there will be a runtime panic. It is not checked during compile time due to Rust
+            /// type bound limits.
+            pub fn convolution<$(const $k: usize, const $b: usize),*>(
+                &self,
+                kernel: &$tensor<$($k),*>,
+                stride: &[usize],
+            ) -> $tensor<$($b),*> where
+                bound!(inner $D<$($k),*>): Sized,
+                bound!(inner $D<$($b),*>): Sized,
+            {
+                $(assert_eq!($b, ($p + $k + stride[$i] - 1) / stride[$i] - 1);)*
+
+                let mut new = Tensor::new_filled(0.0);
+                let mut i = 0;
+
+                convolution!(for_out stride, $($p $k $pv),*; self kernel new i $($p $k $pv $kv),*);
+
+                new
+            }
+        }
+    };
+}
+
+convolution!(HVector Dim1 1 0 W KW BW w kw);
+convolution!(Matrix Dim2 2 0 W KW BW w kw, 1 H KH BH h kh);
+convolution!(Tensor3 Dim3 3 0 W KW BW w kw, 1 H KH BH h kh, 2 D KD BD d kd);
+convolution!(Tensor4 Dim4 4 0 X KX BX x kx, 1 Y KY BY y ky, 2 Z KZ BZ z kz, 3 W KW BW w kw);
+convolution!(Tensor5 Dim5 5 0 X KX BX x kx, 1 Y KY BY y ky, 2 Z KZ BZ z kz, 3 W KW BW w kw, 4 V KV BV v kv);
+convolution!(Tensor6 Dim6 6 0 X KX BX x kx, 1 Y KY BY y ky, 2 Z KZ BZ z kz, 3 W KW BW w kw, 4 V KV BV v kv, 5 U KU BU u ku);
+
 #[cfg(test)]
 #[test]
 fn matrix_mul() {
@@ -345,4 +407,24 @@ fn add_assi() {
 
     a += 1.0;
     assert_eq!(a, vector!(3 [7.0, 8.0, 4.0]));
+}
+
+#[cfg(test)]
+#[test]
+fn convolution() {
+    let a = hvector!(8 [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0]);
+    let b = hvector!(3 [0.5, 0.2, 0.3]);
+    assert_eq!(a.convolution(&b, &[1]), hvector!(10 [0.5, 0.7, 1.0, 0.5, 0.3, 0.0, 0.5, 0.7, 0.5, 0.3]));
+
+    let a = hvector!(8 [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0]);
+    let b = hvector!(4 [0.5, 0.2, 0.3, 0.1]);
+    assert_eq!(a.convolution(&b, &[1]), hvector!(11 [0.5, 0.7, 1.0, 0.6, 0.4, 0.1, 0.5, 0.7, 0.5, 0.4, 0.1]));
+
+    let a = hvector!(10 [3.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0]);
+    let b = hvector!(5 [0.5, 0.2, 0.3, 0.1, 0.2]);
+    assert_eq!(a.convolution(&b, &[1]), hvector!(14 [1.5, 1.1, 1.6, 0.8, 1.0, 0.3, 0.7, 0.7, 0.5, 0.9, 0.5, 0.5, 0.1, 0.2]));
+
+    let a = hvector!(8 [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0]);
+    let b = hvector!(3 [0.5, 0.2, 0.3]);
+    assert_eq!(a.convolution(&b, &[2]), hvector!(5 [0.5, 1.0, 0.3, 0.5, 0.5]));
 }
